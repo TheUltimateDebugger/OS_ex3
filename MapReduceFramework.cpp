4a1472217928f9351d8fc0527551441e688f1df0
TomerGambasho <122287569+TomerGambasho@users.noreply.github.com>
2024-07-08 17:46:47 +0300

    Current progress

--- MapReduceFramework.cpp
+++ MapReduceFramework.cpp
@@ -6,6 +6,7 @@
 #include <semaphore.h>
 #include <algorithm>
 
+struct JobContext;
 
 struct ThreadContext {
     JobState* job_state;
@@ -15,17 +16,17 @@ struct ThreadContext {
     std::atomic<int>* atomic_index;
     pthread_t thread_id;
     const InputVec& input_vec;
-    std::vector<IntermediateVec> intermediate_super_vector;
-    std::vector<IntermediateVec> intermediate_shuffled_super_vector;
+    std::vector<IntermediateVec> &intermediate_super_vector;
+    std::vector<IntermediateVec> &intermediate_shuffled_super_vector;
     OutputVec& output_vec;
     Barrier* barrier;
-    ThreadContext(const MapReduceClient& client, std::atomic<int>* index,
-                  const InputVec& inputVec, OutputVec& outputVec, JobState*
-    job_state1, Barrier*
-    barrier_1, int i, pthread_mutex_t *lock)
-            : job_state(job_state1), client(client), atomic_index(index),
-              input_vec(inputVec), output_vec
-                      (outputVec), barrier(barrier_1), thread_index(i), grade_lock(lock) {}
+    JobContext* job_context;
+    ThreadContext(const MapReduceClient& client, std::atomic<int>* index, const InputVec& inputVec, OutputVec& outputVec,
+                  JobState* job_state1, Barrier* barrier_1, int i, pthread_mutex_t *lock, std::vector<IntermediateVec> &
+                  inter_vector, std::vector<IntermediateVec> & shuffled_vector, JobContext* jc)
+            : job_state(job_state1), client(client), atomic_index(index), input_vec(inputVec), output_vec(outputVec),
+            barrier(barrier_1), thread_index(i), grade_lock(lock), intermediate_super_vector(inter_vector),
+            intermediate_shuffled_super_vector(shuffled_vector), job_context(jc) {}
 };
 
 struct JobContext
@@ -38,7 +39,6 @@ struct JobContext
     pthread_mutex_t job_mutex;
     pthread_cond_t job_cv;
     bool job_done;
-    Barrier *barrier;
 
     JobContext(const InputVec &inputVec, OutputVec &outputVec, JobState *job_state1, int multiThreadLevel)
             : job_state(job_state1), input_vec(inputVec), output_vec(outputVec), job_done(false)
@@ -46,7 +46,6 @@ struct JobContext
         length = inputVec.size();
         pthread_mutex_init(&job_mutex, nullptr);
         pthread_cond_init(&job_cv, nullptr);
-        barrier = new Barrier(multiThreadLevel); // Initialize the barrier
     }
 
     ~JobContext()
@@ -57,7 +56,6 @@ struct JobContext
         }
         pthread_mutex_destroy(&job_mutex);
         pthread_cond_destroy(&job_cv);
-        delete barrier; // Clean up the barrier
     }
 };
 
@@ -120,19 +118,19 @@ void* Boss_thread(void* arg){
         }
         if (max_key == nullptr)
             break;
-        IntermediatePair smallest_pair_transfered = nullptr;
-        for (auto& vec : vectors)
+        K2* smallest_pair_transfered = nullptr;
+        for (auto& vec : tc->intermediate_super_vector)
         {
             if (!vec.empty())
             {
                 IntermediatePair& last_pair = vec.back();
-                if (!(*current_key < *max_key && *max_key < *current_key))
+                if (!(*last_pair.first < *max_key && *max_key < *last_pair.first))
                 {
-                    if (smallest_pair_transfered == nullptr ||
-                    smallest_pair_transfered > last_pair)
+                    if (smallest_pair_transfered == nullptr || *last_pair.first < *smallest_pair_transfered)
                     {
                         tc->intermediate_shuffled_super_vector.push_back
                             ({last_pair});
+                        smallest_pair_transfered = last_pair.first;
                     }
                     else
                     {
@@ -158,15 +156,16 @@ void* Boss_thread(void* arg){
       if (old_value >= tc->input_vec.size())
           break;
 
-      tc->client.reduce(tc->intermediate_shuffled_super_vector[old_value], arg);
+      tc->client.reduce(&tc->intermediate_shuffled_super_vector[old_value], arg);
       //updates percentage, no one can touch it in this time
       pthread_mutex_lock(tc->grade_lock);
       tc->job_state->percentage = ((float)old_value / (float)tc->input_vec.size()) *
           100;
       pthread_mutex_unlock(tc->grade_lock);
     }
+
     //TODO:implement delete
-    return;
+    return nullptr;
 }
 
 void* Minion_thread(void* arg)
@@ -197,22 +196,20 @@ void* Minion_thread(void* arg)
     tc->barrier->barrier();
     //wait for boss to finish shuffling
     tc->barrier->barrier();
-    //TODO: implement reduce
-  while(true)
-  {
+    while(true)
+    {
     int old_value = (*(tc->atomic_index))++;
     if (old_value >= tc->input_vec.size())
       break;
 
-    tc->client.reduce(tc->intermediate_shuffled_super_vector[old_value], arg);
+    tc->client.reduce(&tc->intermediate_shuffled_super_vector[old_value], arg);
     //updates percentage, no one can touch it in this time
     pthread_mutex_lock(tc->grade_lock);
     tc->job_state->percentage = ((float)old_value / (float)tc->input_vec.size()) *
                                 100;
     pthread_mutex_unlock(tc->grade_lock);
-  }
-
-    return;
+    }
+    return nullptr;
 }
 
 JobHandle startMapReduceJob(const MapReduceClient &client, const InputVec &inputVec, OutputVec &outputVec, int multiThreadLevel)
@@ -238,8 +235,10 @@ JobHandle startMapReduceJob(const MapReduceClient &client, const InputVec &input
     // Create and start threads
     for (int i = 0; i < multiThreadLevel; ++i)
     {
-        ThreadContext *thread_context = new ThreadContext(client, atomic_index, inputVec, outputVec,
-                                                          job_state, barrier, i, grade_lock);
+        std::vector<IntermediateVec>* inter_vec = new std::vector<IntermediateVec>;
+        std::vector<IntermediateVec>* shuffle_vec = new std::vector<IntermediateVec>;
+        ThreadContext *thread_context = new ThreadContext(client, atomic_index, inputVec, outputVec, job_state, barrier,
+                                                          i, grade_lock, *inter_vec, *shuffle_vec, job_context);
         job_context->thread_contexts.push_back(thread_context);
         if (i == 0)
         {
@@ -280,6 +279,21 @@ void getJobState(JobHandle job, JobState *state)
     *state = *(jc->job_state);
 }
 
+void MemoryFree(JobContext* jc)
+{
+    // Free memory allocated for thread contexts
+    for (auto &tc : jc->thread_contexts)
+    {
+        // Free intermediate and shuffled vectors
+        delete &tc->intermediate_super_vector;
+        delete &tc->intermediate_shuffled_super_vector;
+        delete &tc->barrier;
+        delete &tc->
+
+        // Free thread context itself
+        delete tc;
+    }
+}
 
 void closeJobHandle(JobHandle job)
 {
@@ -304,7 +318,6 @@ void closeJobHandle(JobHandle job)
 
     // Clean up resources
     delete job_context->job_state;
-    delete job_context->barrier; // Clean up the barrier
 
     // The destructor of JobContext will clean up thread contexts and destroy the mutex and condition variable
     delete job_context;
