774a40ebe25495c35b2f9b84dbe7ace3891ba3c3
TomerGambasho <122287569+TomerGambasho@users.noreply.github.com>
2024-07-08 11:11:36 +0300

    C file

--- MapReduceFramework.cpp
+++ MapReduceFramework.cpp
@@ -1,13 +1,43 @@
 #include "MapReduceFramework.h"
+#include "MapReduceClient.h"
+#include <atomic>
+#include <pthread.h>
+#include <semaphore.h>
 
-type std::vector<std::pair<K1*,V1*>>;
-type std::vector<std::pair<K2*,V2*>>;
-type std::vector<std::pair<K3,V3*>>;
 
-void emit2 (K2* key, V2* value, void* context);
-void emit3 (K3* key, V3* value, void* context);
+struct ThreadContext{
+    MapReduceClient& client;
+    std::atomic<int>* atomic_counter;
+    pthread_t thread_id;
 
-JobHandle startMapReduceJob(const MapReduceClient& client, const InputVec& inputVec, OutputVec& outputVec, int multiThreadLevel);
+
+
+};
+
+
+void emit2 (K2* key, V2* value, void* context){
+
+}
+
+void emit3 (K3* key, V3* value, void* context){
+
+}
+
+void* MapReduce(void* arg){
+    return NULL;
+}
+
+JobHandle startMapReduceJob(const MapReduceClient& client, const InputVec& inputVec, OutputVec& outputVec, int multiThreadLevel){
+//    Create a ThreadContext....
+    std::atomic<int>* atomic_counter(0);
+    ThreadContext* context = new ThreadContext(client, atomic_counter, 0, );
+
+
+    for (int i = 0; i < multiThreadLevel; ++i)
+    {
+        pthread_create(&(context->thread_id), nullptr, MapReduce, context);
+    }
+}
 
 void waitForJob(JobHandle job);
 
