1776667521a9a973ca153bc6f8d8ad8aa4d119dd
TomerGambasho <122287569+TomerGambasho@users.noreply.github.com>
2024-07-08 13:29:31 +0300

    Current progress

--- MapReduceFramework.cpp
+++ MapReduceFramework.cpp
@@ -6,35 +6,49 @@
 #include <semaphore.h>
 
 
-typedef struct ThreadContext{
-    MapReduceClient& client;
+struct ThreadContext {
+    const MapReduceClient& client;
     std::atomic<int>* atomic_index;
     std::atomic<int>* atomic_progress;
     pthread_t thread_id;
-    InputPair input_pair;
-    IntermediatePair intermediate_pair;
-    OutputPair output_pair;
-}ThreadContext;
+    const InputVec& input_vec;
+    IntermediateVec intermediate_vector;
+    OutputVec& output_vec;
 
-typedef struct JobContext{
+    ThreadContext(const MapReduceClient& client, std::atomic<int>* index, std::atomic<int>* progress, const InputVec& inputVec, OutputVec& outputVec)
+            : client(client), atomic_index(index), atomic_progress(progress), input_vec(inputVec), output_vec(outputVec) {}
+};
+
+struct JobContext {
     JobState job_state;
     int length;
-    InputVec& input_vec;
-    IntermediateVec intermediate_vec;
+    const InputVec& input_vec;
     OutputVec& output_vec;
-}JobContext;
+    std::vector<ThreadContext*> thread_contexts;
 
+    JobContext(const InputVec& inputVec, OutputVec& outputVec)
+            : input_vec(inputVec), output_vec(outputVec) {
+        job_state = {UNDEFINED_STAGE, 0};
+        length = inputVec.size();
+    }
 
-void emit2 (K2* key, V2* value, void* context){
-    ThreadContext *tc = (ThreadContext*) context;
-    tc->intermediate_vec.push_back(IntermediatePair(key, value));
-    int old_value = (*(tc->atomic_progress))++;
+    ~JobContext() {
+        for (auto& tc : thread_contexts) {
+            delete tc;
+        }
+    }
+};
+
+void emit2(K2* key, V2* value, void* context) {
+    ThreadContext* tc = (ThreadContext*)context;
+    tc->intermediate_vector.push_back(IntermediatePair(key, value));
+    (*(tc->atomic_progress))++;
 }
 
-void emit3 (K3* key, V3* value, void* context){
-    ThreadContext* tc = (ThreadContext*) context;
+void emit3(K3* key, V3* value, void* context) {
+    ThreadContext* tc = (ThreadContext*)context;
     tc->output_vec.emplace_back(key, value);
-    int old_value = (*(tc->atomic_progress))++;
+    (*(tc->atomic_progress))++;
 }
 
 void* Boss_thread(void* arg){
@@ -46,16 +60,23 @@ void* Minion_thread(void* arg){
 }
 
 JobHandle startMapReduceJob(const MapReduceClient& client, const InputVec& inputVec, OutputVec& outputVec, int multiThreadLevel){
-//    Create a ThreadContext....
+    // Create a ThreadContext....
     std::atomic<int>* atomic_index(0);
     std::atomic<int>* atomic_progress(0);
-    ThreadContext* context = new ThreadContext(client, atomic_counter, 0, inputVec, nullptr, outputVec);
+    JobContext* job_context = new JobContext(inputVec, outputVec);
 
-
-    for (int i = 0; i < multiThreadLevel - 1; ++i)
-    {
-        pthread_create(&(context->thread_id), nullptr, MapReduce, context);
+    // Create and start threads
+    for (int i = 0; i < multiThreadLevel; ++i) {
+        ThreadContext* thread_context = new ThreadContext(client, atomic_index, atomic_progress, inputVec, outputVec);
+        job_context->thread_contexts.push_back(thread_context);
+        if (i == 0) {
+            pthread_create(&thread_context->thread_id, nullptr, Boss_thread, thread_context);
+        } else {
+            pthread_create(&thread_context->thread_id, nullptr, Minion_thread, thread_context);
+        }
     }
+
+    return static_cast<JobHandle>(job_context);
 }
 
 void waitForJob(JobHandle job);
