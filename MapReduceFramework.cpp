b13c9dfd38f2295d3a5ec3b4d3713a8acc7648e3
TomerGambasho <122287569+TomerGambasho@users.noreply.github.com>
2024-07-08 12:29:17 +0300

    Current progress

--- MapReduceFramework.cpp
+++ MapReduceFramework.cpp
@@ -9,18 +9,24 @@ struct ThreadContext{
     MapReduceClient& client;
     std::atomic<int>* atomic_counter;
     pthread_t thread_id;
-
-
+    InputVec& input_vec;
+    IntermediateVec intermediate_vec;
+    OutputVec& output_vec;
+    JobState job_state;
 
 };
 
 
 void emit2 (K2* key, V2* value, void* context){
-
+    ThreadContext *tc = (ThreadContext*) context;
+    tc->intermediate_vec.push_back(IntermediatePair(key, value));
+    tc->atomic_counter++;
 }
 
 void emit3 (K3* key, V3* value, void* context){
-
+    ThreadContext* tc = (ThreadContext*) context;
+    tc->output_vec.emplace_back(key, value);
+    tc->atomic_counter++;
 }
 
 void* MapReduce(void* arg){
@@ -30,7 +36,7 @@ void* MapReduce(void* arg){
 JobHandle startMapReduceJob(const MapReduceClient& client, const InputVec& inputVec, OutputVec& outputVec, int multiThreadLevel){
 //    Create a ThreadContext....
     std::atomic<int>* atomic_counter(0);
-    ThreadContext* context = new ThreadContext(client, atomic_counter, 0, );
+    ThreadContext* context = new ThreadContext(client, atomic_counter, 0, inputVec, nullptr, outputVec);
 
 
     for (int i = 0; i < multiThreadLevel; ++i)
